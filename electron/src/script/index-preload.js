4cd9a88840c9fe11e46e2b25b65807be60716eaa
Gabriel Bastianelle <gsbastianelle@gmail.com>
2024-03-04 02:28:11 +0000

    refactor services

--- electron/src/script/index-preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("myName", {
-    myFunction: (callback) =>
-        ipcRenderer.on("client-socket", (_event, value) => callback(value)),
-});
--- gui/app/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Home() {
-    return <main>mudani page</main>;
-}
